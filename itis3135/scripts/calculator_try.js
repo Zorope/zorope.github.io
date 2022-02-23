e04209f15c45cfb9b84f971439aff3ee8507ab4f
Zorope <71614711+Zorope@users.noreply.github.com>
2022-02-22 20:04:03 -0500

    separation of concern

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -2,48 +2,33 @@ var firstnum = 0;
 var secondnum = 0;
 var action = "";
 //Updates calculator screen to correct numbers
-function calcScreen(character)
+function calcScreen()
 {
     //Page keeps refreshing whenever button is pressed
-    document.getElementById("screen").value = document.getElementById("screen").value + character;
+    document.getElementById("screen").value = document.getElementById("screen").value + this.value;
 }
 //Adds numbers to screen
-function calculator(character)
+window.onload = function ()
 {
-    var thing = parseInt(character);
-    switch(thing)
-    {
-        case 0:
-            calcScreen("0");
-            break;
-        case 1:
-            calcScreen("1");
-            break;
-        case 2:
-            calcScreen("2");
-            break;
-        case 3:
-            calcScreen("3");
-            break;
-        case 4:
-            calcScreen("4");
-            break;
-        case 5:
-            calcScreen("5");
-            break;
-        case 6:
-            calcScreen("6");
-            break;
-        case 7:
-            calcScreen("7");
-            break;
-        case 8:
-            calcScreen("8");
-            break;
-        case 9:
-            calcScreen("9");
-            break;
-    }
+    document.getElementById("button1").onclick = calcScreen;
+    document.getElementById("button2").onclick = calcScreen;
+    document.getElementById("button3").onclick = calcScreen;
+    document.getElementById("button4").onclick = calcScreen;
+    document.getElementById("button5").onclick = calcScreen;
+    document.getElementById("button6").onclick = calcScreen;
+    document.getElementById("button7").onclick = calcScreen;
+    document.getElementById("button8").onclick = calcScreen;
+    document.getElementById("button9").onclick = calcScreen;
+    document.getElementById("button0").onclick = calcScreen;
+
+    document.getElementById("buttonDecrement").onclick = decrement;
+    document.getElementById("buttonDivide").onclick = divide;
+    document.getElementById("buttonIncrement").onclick = increment;
+    document.getElementById("buttonAdd").onclick = add;
+    document.getElementById("buttonMinus").onclick = subtract;
+    document.getElementById("buttonClear").onclick = clean;
+    document.getElementById("buttonMultiply").onclick = multiply;
+    document.getElementById("buttonCalc").onclick = calculate;
 }
 //function to add
 function add()
@@ -77,6 +62,22 @@ function divide()
     document.getElementById("screen").value = "";
     action = "divide";
 }
+//Decrements the number
+function decrement()
+{
+    var number = parseInt(document.getElementById("screen").value);
+    firstnum = number;
+    firstnum--;
+    document.getElementById("screen").value = firstnum;
+}
+//Increments the number
+function increment()
+{
+    var number = parseInt(document.getElementById("screen").value);
+    firstnum = number;
+    firstnum++;
+    document.getElementById("screen").value = firstnum;
+}
 //clears calculator
 function clean()
 {
@@ -94,27 +95,27 @@ function calculate()
         secondnum = parseInt(number2);
         document.getElementById("screen").value = "";
         var result = firstnum + secondnum;
-        calcScreen(result);
+        document.getElementById("screen").value = result;
     }
     if(action == "subtract")
     {
         secondnum = parseInt(number2);
         document.getElementById("screen").value = "";
         var result = firstnum - secondnum;
-        calcScreen(result);
+        document.getElementById("screen").value = result;
     }
     if(action == "multiply")
     {
         secondnum = parseInt(number2);
         document.getElementById("screen").value = "";
         var result = firstnum * secondnum;
-        calcScreen(result);
+        document.getElementById("screen").value = result;
     }
     if(action == "divide")
     {
         secondnum = parseInt(number2);
         document.getElementById("screen").value = "";
         var result = firstnum / secondnum;
-        calcScreen(result);
+        document.getElementById("screen").value = result;
     }
 }
