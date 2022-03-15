144795875d70ceea9c37f7c852fc5b253206de12
Zorope <71614711+Zorope@users.noreply.github.com>
2022-03-15 19:48:23 -0400

    images and coding fix

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -3,12 +3,16 @@ $(document).ready(function()
 {
     //Variable to hold link for image
     var slide;
+    //Variable to hold text for a caption
+    var caption;
     //When button is clicked runs a function
     $("button").click(function()
     {
         //Makes slide equal to image that button is holding
         slide = $(this).attr("href");
+        caption = $(this).attr("id");
         //Displays image to the page
         $("#my_image").attr("src", slide);
+        $("#my_caption").text(caption);
     });
 });
