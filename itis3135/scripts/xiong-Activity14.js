c7ec23921863ca0c951de2aba49caf142a4dc126
Zorope <71614711+Zorope@users.noreply.github.com>
2022-04-12 13:26:36 -0400

    Activity 14 progress

--- itis3135/scripts/xiong-Activity14.js
+++ itis3135/scripts/xiong-Activity14.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+	$("#nav_list li").click(function()
+    {
+        var section = $(this).children("a").attr("title");
+        $.ajax({
+            type: "get",
+            url: "json_files/" + section + ".json",
+            //I don't know what to do.
+        });
+    });
 }); // end ready
