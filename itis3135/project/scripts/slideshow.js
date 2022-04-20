82ff56ab665165b90bbab989dcdd7ac25dd78b8b
Zorope <71614711+Zorope@users.noreply.github.com>
2022-04-20 13:52:13 -0400

    project updates

---
+++ itis3135/project/scripts/slideshow.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      minSlides: 1,
+      maxSlides: 1,
+      slideWidth: 700,
+      adaptiveHeight: true,
+      captions: true,
+      speed: 1000,
+      auto: true
+    });
+  });
