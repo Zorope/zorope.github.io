58b2d1336d37cff2d25f1fccb986afdd931f2523
Zorope <71614711+Zorope@users.noreply.github.com>
2022-04-20 15:32:39 -0400

    Update xiong-Activity16.js

--- itis3135/scripts/xiong-Activity16.js
+++ itis3135/scripts/xiong-Activity16.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     //Got from chapter 12 slides
-    var url = "https://api.flickr.com/services/feed/photos_public.gne?" + "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorp";
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
     $.getJSON(url, function(data){
         var html = "";
         $.each(data.items, function(i, item){
-            html += '<img src="' + item.media.m + '">';
+            html += "<img src=" + item.media.m + ">";
         });
-        $("#new_building").html(html);
-    })
+    $("#new_building").html(html);
+    });
 });
